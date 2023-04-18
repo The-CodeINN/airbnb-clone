3f0c4eac7fe9d87046780dab4995e3c2a8af1b57
The-CodeINN <emijere.richard@gmail.com>
2023-04-18 15:31:28 +0100

    Added the Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,28 @@
-import './globals.css'
+import './globals.css';
+import Navbar from './components/Navbar/Navbar';
 
-import {Work_Sans} from 'next/font/google'
+import { Work_Sans } from 'next/font/google';
 
 export const metadata = {
   title: 'Air BnB Clone',
   description: 'Air BnB Clone built with Next.js and Tailwind CSS',
-}
+};
 
 const font = Work_Sans({
   subsets: ['latin'],
-})
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body
-        className={font.className}
-      >{children}</body>
+    <html lang='en'>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
